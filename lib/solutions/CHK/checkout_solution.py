6b92118b55a95c15df3b9f3dc502a9bc3d2703c6
sbx_user1051 <sbx_user1051@169.254.22.197>
2021-10-02 11:10:52 +0000

    Sat Oct 02 11:10:52 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,7 +1,8 @@
-
-
 # noinspection PyUnusedLocal
 # skus = unicode string
+from enum import Enum
+
+
 def checkout(skus: str):
     basket = {}
     for sku in list(skus):
@@ -11,8 +12,21 @@ def checkout(skus: str):
     return 0
 
 
+class Good(Enum):
+    A = "A"
+    B = "B"
+    C = "C"
+    D = "D"
+
+
 class Basket:
 
     def __init__(self) -> None:
-        super().__init__()
+        self.items = {}
 
+    def add_item(self, good: Good):
+        count = self.items.get(good)
+        if count is None:
+            count = 0
+        count += 1
+        self.items[good] = count
