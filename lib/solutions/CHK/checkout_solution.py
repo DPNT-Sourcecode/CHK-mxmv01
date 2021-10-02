56b2518979a48294c9ae880996e96305b5ef0aa9
sbx_user1051 <sbx_user1051@169.254.22.197>
2021-10-02 11:07:52 +0000

    Sat Oct 02 11:07:52 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,7 +2,17 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
+def checkout(skus: str):
+    basket = {}
+    for sku in list(skus):
+        count = basket.get(sku)
+
     print(skus)
     return 0
+
+
+class Basket:
+
+    def __init__(self) -> None:
+        super().__init__()
 
