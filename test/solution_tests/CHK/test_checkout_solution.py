21bb5dfd2d8944be98ff2a66bc4422395cedc6c1
sbx_user1051 <sbx_user1051@169.254.22.197>
2021-10-02 12:28:38 +0000

    Sat Oct 02 12:28:38 UTC 2021

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -22,9 +22,10 @@ class TestCheckout:
         assert checkout("AAABB") == 175
 
     def test_applies_discount_to_correct_number_of_product(self):
-        assert checkout("AAAAABBB") == 305
+        assert checkout("AAAAABBB") == 275
 
     def test_applies_bigger_discounts_before_smaller(self):
         assert checkout("AAAAAAAAA") == 380
+
 
 
